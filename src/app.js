0e1edd15d83197e3a74cebe908fe0e80fdd99240
the-vagab0nd <lit2018008@iiitl.ac.in>
2021-04-23 04:34:22 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -94,4 +94,4 @@ app.get('*', (req, res) => res.render('404', {
     title: '404',
 }))
 
-app.listen(3000, () => console.log('Ran successfully'))
+app.listen(port, () => console.log('Ran successfully'))
